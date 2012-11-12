d02423fbd9b86ff6b5f9f762ffd738bea0da7e18
Dean Sofer <proloser@hotmail.com>
2012-11-12 15:02:04 -0800

    TinyMCE now merges custom callbacks with directive-implemented callbacks

--- modules/directives/tinymce/tinymce.js
+++ modules/directives/tinymce/tinymce.js
@@ -6,45 +6,53 @@ angular.module('ui.directives').directive('uiTinymce', ['ui.config', function (u
   return {
     require: 'ngModel',
     link: function (scope, elm, attrs, ngModel) {
-      var expression,
-        options = {
-          // Update model on button click
-          onchange_callback: function (inst) {
-            if (inst.isDirty()) {
-              inst.save();
-              ngModel.$setViewValue(elm.val());
-              if (!scope.$$phase)
-                scope.$apply();
-            }
-          },
-          // Update model on keypress
-          handle_event_callback: function (e) {
-            if (this.isDirty()) {
-              this.save();
-              ngModel.$setViewValue(elm.val());
-              if (!scope.$$phase)
-                scope.$apply();
-            }
-            return true; // Continue handling
-          },
-          // Update model when calling setContent (such as from the source editor popup)
-          setup: function (ed) {
-            ed.onSetContent.add(function (ed, o) {
-              if (ed.isDirty()) {
-                ed.save();
-                ngModel.$setViewValue(elm.val());
-                if (!scope.$$phase)
-                  scope.$apply();
-              }
-            });
-          }
-        };
+      var expression, onchanceCallback, handleEventCallback, setup,
+        options = {};
       if (attrs.uiTinymce) {
         expression = scope.$eval(attrs.uiTinymce);
       } else {
         expression = {};
       }
       angular.extend(options, uiConfig.tinymce, expression);
+
+      // Update model on button click
+      onchanceCallback = options.onchange_callback || angular.noop;
+      options.onchange_callback = function (inst) {
+        if (inst.isDirty()) {
+          inst.save();
+          ngModel.$setViewValue(elm.val());
+          if (!scope.$$phase)
+            scope.$apply();
+        }
+        return onchanceCallback(inst);
+      };
+
+      // Update model on keypress
+      handleEventCallback = options.handle_event_callback || function(){ /* Continue handling */ return true; };
+      options.handle_event_callback = function (e) {
+        if (this.isDirty()) {
+          this.save();
+          ngModel.$setViewValue(elm.val());
+          if (!scope.$$phase)
+            scope.$apply();
+        }
+        return handleEventCallback(e);
+      };
+
+      // Update model when calling setContent (such as from the source editor popup)
+      setup = options.setup || angular.noop;
+      options.setup = function (ed) {
+        ed.onSetContent.add(function (ed, o) {
+          if (ed.isDirty()) {
+            ed.save();
+            ngModel.$setViewValue(elm.val());
+            if (!scope.$$phase)
+              scope.$apply();
+          }
+        });
+        return setup(ed);
+      };
+
       setTimeout(function () {
         elm.tinymce(options);
       });
